8fbe051f36d300fd30966f28c5a81a6e9c23f5a3
Haley Goodfield <haleygoodfield@usf.edu>
2025-03-11 18:53:54 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,6 @@
+// Task 1: Creating the Base Structure
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Risk Dashboard Loaded"); // // Print "Risk Dashboard Loaded" to the console.
+    const riskDashboard = document.getElementById("riskDashboard"); // select the riskDashboard container
+});
+    
